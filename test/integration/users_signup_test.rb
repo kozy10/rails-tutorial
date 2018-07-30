0535465a10d33f9c214f8a32f0a20e6bc4b984e7
kozy10 <kentaro1002vb@gmail.com>
2018-07-30 14:10:15 +0000

    Implementbasic login

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -10,7 +10,9 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_template 'users/new'
     assert_select 'div#error_explanation'
     assert_select 'div.field_with_errors'
+  end
     
+  test "valid signup information" do
     get signup_path 
     assert_difference 'User.count', 1 do
       post users_path, params: { user: { name: "Example User", email: "user@example.com",
@@ -19,5 +21,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     follow_redirect!
     assert_template 'users/show'
     assert_not flash.empty?
+    assert is_logged_in?
   end
 end
