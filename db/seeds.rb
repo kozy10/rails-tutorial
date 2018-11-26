b0a67d428cf180ece0aca6daab57e58506f5fe40
EC2 Default User <ec2-user@ip-172-31-1-16.ap-southeast-1.compute.internal>
2018-11-26 07:39:36 +0000

    Add user microposts

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
 User.create!(name: "Example User",
   email: "example@railstutorial.org",
   password: "foobar",
@@ -13,6 +5,7 @@ User.create!(name: "Example User",
   admin: true,
   activated: true,
   activated_at: Time.zone.now)
+
   
 99.times do |n|
   name = Faker::Name.name
@@ -25,4 +18,10 @@ User.create!(name: "Example User",
     activated: true,
     activated_at: Time.zone.now)
 end
-  
+
+users = User.order(:created_at).take(6)
+50.times do
+  content = Faker::Lorem.sentence(5)
+  users.each { |user| user.microposts.create!(content: content) }
+end
+
