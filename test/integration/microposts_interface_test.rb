ab490f63359b9e8255050dab915234ddcb12562a
EC2 Default User <ec2-user@ip-172-31-1-16.ap-southeast-1.compute.internal>
2018-11-27 08:58:23 +0000

    Add user following

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -8,6 +8,8 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
   test "micropost interface" do
     log_in_as(@user)
     get root_path
+    assert_match @user.active_relationships.count.to_s, response.body
+    assert_match @user.passive_relationships.count.to_s, response.body
     assert_select 'div.pagination'
     assert_select 'input[type=file]'
     post microposts_path, params: { micropost: { content: ""}}
